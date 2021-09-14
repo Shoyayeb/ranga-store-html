c06025b6ae90c4b181fe6efd63a8a7f0a3fce8a3
Shoyayeb <sa.fine@outlook.com>
2021-09-14 21:10:34 +0600

    comment added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+// fetchinig products data
 const loadProducts = () => {
   fetch('https://fakestoreapi.com/products')
     .then(res => res.json())
@@ -6,7 +6,7 @@ const loadProducts = () => {
   // console.log(json);
 };
 
-// show all product in UI 
+// showing all product in UI 
 const showProducts = (products) => {
   console.log(products);
   const allProducts = products.map((pd) => pd);
@@ -14,32 +14,34 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    // show details modals text
+    // showing cards for products
     div.innerHTML = `
     <div class="col ">
-    <div class="single-product rounded-3 card bg-warning bg-opacity-10">
-      <img class="card-img-top mx-auto imgCard" src=${image}></img>
-      <div class="card-body">
-        <h4 class="card-title">${product.title.slice(0, 60)}</h4>
-        <p>Category: ${product.category}</p>
-      </div>
-      <div class="card-footer ">
-        <h3>Price: $ ${product.price}</h3>
-        <div class="d-flex flex-column gap-3">
-          <div id="star${product.id}" class="d-flex justify-content-center">
-          </div>
-          <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn"
-            class="buy-now btn btn-success">add to cart</button>
-          <button id="details-btn" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
+      <div class="single-product rounded-3 card bg-warning bg-opacity-10">
+        <img class="card-img-top mx-auto imgCard" src=${image}></img>
+        <div class="card-body">
+          <h4 class="card-title">${product.title.slice(0, 60)}</h4>
+          <p>Category: ${product.category}</p>
+        </div>
+        <div class="card-footer ">
+          <h3>Price: $ ${product.price}</h3>
+          <div class="d-flex flex-column gap-3">
+            <div id="star${product.id}" class="d-flex justify-content-center">
+
+            </div>
+            <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn"
+              class="buy-now btn btn-success">add to cart</button>
+            <button id="details-btn" class="btn btn-outline-info">Details</button>
+            </div>
         </div>
       </div>
-    </div>
                 `;
     document.getElementById("all-products").appendChild(div);
     addStar(Math.round(product.rating.rate), product.id, product.rating.count, product.rating.rate)
   }
 };
-function addStar(starCount, id, rateTotal,rateAvarage) {
+// function for rating stars
+function addStar(starCount, id, rateTotal, rateAvarage) {
   const starid = 'star' + id;
   let i = 0;
   while (i < starCount) {
@@ -51,8 +53,6 @@ function addStar(starCount, id, rateTotal,rateAvarage) {
     i++;
 
   }
-  // const staridd = document.getElementById(starid)
-  // console.log(staridd.childElementCount + ' ' + starid + ' ' + starCount);
   const starOnWeb = document.getElementById(starid).childElementCount;
   for (let i = starOnWeb; i < 5; i++) {
     const noStar = document.createElement("div");
@@ -61,7 +61,6 @@ function addStar(starCount, id, rateTotal,rateAvarage) {
     `
     document.getElementById(starid).appendChild(noStar);
   }
-  // ratingCountShow(count)
   const countOnWeb = document.createElement("div")
   countOnWeb.innerHTML = `
   <div> 
@@ -71,6 +70,7 @@ function addStar(starCount, id, rateTotal,rateAvarage) {
   `
   document.getElementById(starid).appendChild(countOnWeb);
 }
+// function for updating cart
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -79,14 +79,14 @@ const addToCart = (id, price) => {
   updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
-
+//function for getting input value
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
   const converted = parseFloat(element);
   return converted;
 };
 
-// main price update function
+//function for main price update
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
@@ -94,7 +94,7 @@ const updatePrice = (id, value) => {
   document.getElementById(id).innerText = total.toFixed(2);
 };
 
-// set innerText function
+//function for set innerText
 const setInnerText = (id, value) => {
   document.getElementById(id).innerText = value.toFixed(2);
 };
@@ -116,7 +116,7 @@ const updateTaxAndCharge = () => {
   }
 };
 
-//grandTotal update function
+//function for grandTotal update
 const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
@@ -124,7 +124,7 @@ const updateTotal = () => {
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
 
-// resetting value to 0 when clicking buy button
+//function for resetting value to 0 when clicking buy button
 function resetValue() {
   console.log("hi");
 }
